184d6412dd6b7f35cbbc087d981ed42594cdd306
MAST3RZ3R0123 <mdipaajisaka@gmail.com>
2026-07-21 14:16:13 +0700

    Add files via upload

---
+++ Dipa.js
@@ -0,0 +1,3 @@
+console.log("nama : Mohamad Dipa Ajisaka Rabbani");
+console.log("SMP : SMP Islam Sabilillah Malang");
+console.log("alasan : biar bisa mendapat kerja dengan mudah dan bisa diterima di perusahaan yang saya inginkan");
